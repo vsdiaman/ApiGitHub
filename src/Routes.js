03bcca971285d175e1b025e6fdb00a61e89b52d6
vsdiaman <vitor.diamantino8@gmail.com>
2021-12-13 15:41:40 -0300

    projeto base

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as BRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+
+const Routes = () => (
+  <BRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  </BRouter>
+);
+
+export default Routes;
